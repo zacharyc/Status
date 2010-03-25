8169632c6f93c5e4dfd04fb32e68bde7a56abd6f
Zachary Cohen <zachary@zacharyc.com>
2010-03-25 00:26:24 -0700

    Added code for project routes. Started Rspec for project

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -2,12 +2,21 @@ require 'spec_helper'
 
 describe Project do
   before(:each) do
-    @valid_attributes = {
-      
-    }
+    @project = Project.create(:name => "Test Project")
   end
 
   it "should create a new instance given valid attributes" do
     Project.create!(@valid_attributes)
+  end
+  
+  describe "given notes" do
+    before(:each) do
+      @note = Note.create(:content => "This is a brand new note")
+    end
+    
+    it "should be able to add a note" do
+      @project.notes << @note
+      @project.save()
+    end
   end
 end
