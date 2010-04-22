acc6d42b330c4d997935d709067ce5896c0e8c26
Zachary Cohen <zachary@zacharyc.com>
2010-04-21 18:54:56 -0700

    Strated to work on basic configuration and added home icon.

--- config/environment.rb
+++ config/environment.rb
@@ -38,4 +38,8 @@ Rails::Initializer.run do |config|
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
+  
+  APP_CONFIG = YAML.load_file("#{RAILS_ROOT}/config/config.yml")
 end
+
+SITE_NAME = "zacharyc"
